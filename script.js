7505a7015145cf682a8974973adb06b2693ac9ae
LucasCreates <lukefowler89@gmail.com>
2023-04-28 23:05:11 +0100

    created a mouseover effect and added a prompt for grid size

--- script.js
+++ script.js
@@ -1,56 +1,58 @@
 document.addEventListener("DOMContentLoaded", function(){
 	createGrid(16)
-	console.log("test")
+	
+	const select = document.querySelector(".select-btn");
+
+	select.addEventListener('click', function(){
+		
+	 	let size = getSize();
+	 	createGrid(size);
+	});
+
+
+	
 })
+
+function getSize(){
+	
+	input = prompt("What will be the size? ");
+	
+	let message = document.querySelector("#message");
+	if (input > 0 && input < 100 ){
+		message.textContent = `You have chosen ${input}`;
+		console.log("provide a number");
+	}
+	else if (input < 0 || input > 100){
+		message.textContent = "Provide a number between 0 and 100";
+		console.log("provide a number between")
+	}
+	else {
+		message.textContent = "That is not a valid option";
+	}
+	return input;
+	
+}
+
 
 function createGrid(size){
 		let board = document.querySelector(".board");
 	    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 	    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
 	    let numDivs = size * size
 
 	    for(let i = 0; i < numDivs; i++){
 	    	let div = document.createElement("div");
 	    	div.style.backgroundColor = "#ededed";
-	    	div.style.border = "1px solid black"
+	    	
 	    	board.insertAdjacentElement("beforeend", div);
+	    	div.addEventListener("mouseover", (event) =>{
+	    		event.target.style.backgroundColor ="black"
+	    	})
 	    }
-	
+		
 };
 
-
-
-
-// const slider = document.querySelector("#slider");
-// const screenValue = document.querySelector("#value");
-
-// slider.addEventListener("input", function(){
-// 	let val = document.getElementById("slider").value;
-// 	screenValue.textContent = val;
-// 	grid.setAttribute('style', `grid-template-columns: repeat(${val}, 2fr); grid-template-rows: repeat(${val}, 2fr); `);
-// 	console.log(firstChild);
-// 	for(let i = 0; i <= val*val; i++){
-// 		const div = document.createElement('div');
-// 		  div.classList.add('cell');
-//     	  div.addEventListener('mouseover', function(event){
-//           event.target.style.backgroundColor = multipleColor();
-//           })
-// 		  grid.appendChild(div); 
-// 	}
-
-// });
-
-
-// function myFunction(){
-// 	squares.style.background = multipleColor();
-// }
-// function multipleColor(){
-// 	color = Math.random().toString(16).slice(2,8);
-// 	return ("#" + color) 
-// }
-
-// createGrid();
-// console.log(multipleColor());
 
 
 
